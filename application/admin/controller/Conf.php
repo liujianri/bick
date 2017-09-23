d89aa8c74461b892475dd4bc7308a49fb9caadb0
liujianri <574906227@qq.com>
2017-09-23 15:46:58 +0800

    no message

--- application/admin/controller/Conf.php
+++ application/admin/controller/Conf.php
@@ -8,15 +8,34 @@ class Conf extends Base
 
     public function lst(){
         $conf = new ConfModel;
-        $conflist=$conf->paginate(5);
+
+        if (request()->isPost()) {
+            $sorts=input('post.');
+            foreach ($sorts as $key => $value) {
+                $conf->update(['id'=>$key,'sort'=>$value]);
+            }
+            $this->redirect('lst');
+            return;
+        }
+        $conflist=$conf->order('sort desc')->paginate(5);
         $this->assign('conflists',$conflist);
         return view();
+    }
+
+    public function confs(){
+        $conf = new ConfModel;
+        $conflist=$conf->order('sort desc')->paginate(5);
+        $this->assign('conflist',$conflist);
+        return view('conf');
     }
 
     public function add(){
 
         if (request()->isPost()) {
             $data = $this->request->post();
+            if ($data['valueres']) {
+                $data['valueres']=str_replace('，', ',', $data['valueres']);
+            }
             $conf = new ConfModel;
             if ($conf->save($data)) {
                 $this->redirect('lst');
@@ -25,5 +44,29 @@ class Conf extends Base
             }
         }
         return view();
+    }
+    public function edit(){
+        $confModel = ConfModel::get(input('id'));
+        if (request()->isPost()) {
+            $data = $this->request->post();
+            if ($data['valueres']) {
+                $data['valueres']=str_replace('，', ',', $data['valueres']);
+            }
+            if (!$confModel->save($data)==false) {
+                $this->redirect('lst');
+            }else{
+                return $this->error('修改失败');
+            }
+            return;
+        }
+        $this->assign('confs',$confModel);
+        return view();
+    }
+    public function del(){
+        if (ConfModel::destroy(input('id'))) {
+            return $this->redirect('lst');
+        }else{
+            return $this->error('删除失败');
+        }
     }
 }
